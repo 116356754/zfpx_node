0e8b1c17753376a8380503ca6a25df7c51e65108
116356754 <116356754@qq.com>
2017-04-10 14:49:31 +0800

    http

--- http/http_server_formidable.js
+++ http/http_server_formidable.js
@@ -5,6 +5,8 @@ var util = require('util');
 var fs = require('fs');
 var querystring = require('querystring');
 
+var formidable = require('formidable');
+
 server.on('request',function(req,res){
     var urlObj = url.parse(req.url,true);
     var pathName = urlObj.pathname;
@@ -29,24 +31,25 @@ server.on('request',function(req,res){
         })
     }
     else if(pathName == '/file'){
-        var out = fs.createWriteStream('./form.txt');
-        req.pipe(out);
-
-        var buffers =[];
-        req.on('data',function(chunk){
-            buffers.push(chunk);
-        });
-        req.on('end',function(){
-            var final = Buffer.concat(buffers);
-            res.writeHead(200, {'Content-Type': 'text/plain; charset=utf8'});
-            res.end(JSON.stringify(obj));
+        var form = new formidable.IncomingForm();
+        form.parse(req,function(err,fields,files){
+            fs.createReadStream(files.avatar.path).pipe(fs.createWriteStream(files.avatar.name));
+            res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+            res.write('<h1>'+fields.username+'</h1>');
+            res.write('<h1>'+fields.email+'</h1>')
+            res.write('<img src='+files['avatar'].name+'/>')
+            res.end();
         })
-
-        res.end('ok');
     }
     else{
-        res.end('404');
+
+        fs.exists('.'+pathName,function(exist){
+            if(exist)
+                fs.createReadStream('.'+pathName).pipe(res);
+            else
+                res.end('404');
+        });
     }
-})
+});
 
 server.listen(8080);
